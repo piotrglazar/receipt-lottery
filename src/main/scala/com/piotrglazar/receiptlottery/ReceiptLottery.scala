d8f09625ded4b525e31e5a827633616389550bbb
Piotr Glazar <piotr.glazar@gmail.com>
2016-07-30 23:54:31 +0200

    Code refactoring

--- src/main/scala/com/piotrglazar/receiptlottery/ReceiptLottery.scala
+++ src/main/scala/com/piotrglazar/receiptlottery/ReceiptLottery.scala
@@ -1,37 +1,21 @@
 package com.piotrglazar.receiptlottery
 
 import akka.actor.ActorSystem
-import akka.util.Timeout
-import com.piotrglazar.receiptlottery.extension.SpringExtension
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import akka.pattern.ask
-import scala.concurrent.Await
-import scala.concurrent.duration._
 
 @SpringBootApplication
 class ReceiptLottery {
 
-  @Autowired
-  def runApplication_=(a: ReceiptLoteryApp): Unit = {
-    a.verifyTokens()
-  }
-
-//  @Autowired
-  def runActors_=(actorSystem: ActorSystem): Unit = {
-    val actor = actorSystem.actorOf(SpringExtension.provider.get(actorSystem).props())
-    implicit val timeout = Timeout(5 seconds)
-    val result = actor ? Token("DX9KHFP6KRT")
-
-    println(Await.result(result, timeout.duration))
-    actorSystem.stop(actor)
-    actorSystem.shutdown()
-    actorSystem.awaitTermination()
-  }
 }
 
 object ReceiptLottery extends App {
 
-  SpringApplication.run(classOf[ReceiptLottery], args: _*)
+  private val applicationContext = SpringApplication.run(classOf[ReceiptLottery], args: _*)
+
+  private val app = applicationContext.getBean(classOf[ReceiptLoteryApp])
+
+  app.verifyTokens()
+
+  applicationContext.getBean(classOf[ActorSystem]).shutdown()
 }
--- src/test/scala/com/piotrglazar/receiptlottery/ReceiptLotteryTest.scala
+++ src/test/scala/com/piotrglazar/receiptlottery/ReceiptLotteryTest.scala
@@ -1,30 +1,56 @@
 package com.piotrglazar.receiptlottery
 
-import akka.actor.ActorSystem
-import org.springframework.beans.factory.annotation.{Qualifier, Autowired}
+import akka.actor.{ActorRef, ActorSystem}
+import akka.util.Timeout
+import akka.pattern.ask
+import com.piotrglazar.receiptlottery.core.TokenReader
+import com.piotrglazar.receiptlottery.extension.SpringExtension
+import org.springframework.beans.factory.annotation.{Autowired, Qualifier}
 import org.springframework.test.context.ContextConfiguration
+import rx.lang.scala.Observable
+
+import scala.concurrent.duration._
 
 @ContextConfiguration(classes = Array[Class[_]](classOf[ReceiptLottery]))
 class ReceiptLotteryTest extends BaseContextTest  {
 
   override def beforeAll(): Unit = {
-    System.setProperty("token.file", "/testTokens.txt")
+    System.setProperty("token.file", "/realTokens.txt")
     System.setProperty("results.page", "https://loteriaparagonowa.gov.pl/wyniki")
     super.beforeAll()
+  }
+
+  override def afterAll(): Unit = {
+    System.setProperty("token.file", "")
+    super.afterAll()
   }
 
   @Autowired
   @Qualifier("actorSystem")
   var actorSystem: ActorSystem = _
 
+  @Autowired
+  var tokenReader: TokenReader = _
+
   "ReceiptLottery" should "use actors to verify tokens" in {
     // given
-    // context loaded
+    val actor = actorSystem.actorOf(SpringExtension.provider.get(actorSystem).props())
 
     // when
-    new ReceiptLottery().runActors_=(actorSystem)
+    val tokens = runActors(actor, tokenReader)
 
     // then
-    System.setProperty("token.file", "")
+    val tokenList = tokens.toBlocking.toList
+    tokenList should have size 1
+
+    // cleanup
+    actorSystem.stop(actor)
+  }
+
+  private def runActors(actor: ActorRef, tokenReader: TokenReader): Observable[VerifiedToken] = {
+    implicit val timeout = Timeout(5 seconds)
+    tokenReader
+      .readTokens()
+      .flatMap(t => Observable.from((actor ? t).mapTo[VerifiedToken])(actorSystem.dispatcher))
   }
 }
